2ec40006159f14ac24b4d88d3d79e91a482d77fa
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-06-05 10:14:04 -0400

    Refactored code to not expose password the correct way

--- routes/session.js
+++ routes/session.js
@@ -21,19 +21,26 @@ router.route('/authenticate')
 	res.json(req.decoded)
 })
 .post((req, res, next) => {
-	const user = search(req.body.username, users)
+	let user = search(req.body.username, users)
 	if (!user) {
 		res.status(404).send('User not found')
 	} else if (user.password !== req.body.password) {
 		res.status(403).send('Bad credentials')
-	} else {
-		delete user.password
-		jwt.sign(user, config.secret, { expiresIn: '7d' }, (err, token) => {
+	} else if (user.password === req.body.password) {
+		let data = {
+			id: user.id,
+			name: user.name,
+			admin: user.admin,
+			username: user.username
+		}
+		jwt.sign(data, config.secret, { expiresIn: '7d' }, (err, token) => {
 			if (err) {
 				res.status(500).send('There was an error creating your token')
 			}
-			res.json({ user: user, token: token })
+			res.json({ user: data, token: token })
 		})
+	} else {
+		res.status(500).send('There was an error creating your token')
 	}
 })
 
